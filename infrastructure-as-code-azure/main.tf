9686940b777517cb7cc9b0e7afb561876e765d7a
vickycastillo76-svg <vickycastillo76@icloud.com>
2026-05-04 14:56:42 +0200

    feat(terraform): architect core network structure including vnet and subnets

---
+++ infrastructure-as-code-azure/main.tf
@@ -0,0 +1,30 @@
+# 1. Grupo de Recursos
+resource "azurerm_resource_group" "hospital_rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = { Environment = "Production", ManagedBy = "Terraform" }
+}
+
+# 2. Red Virtual (VNet)
+resource "azurerm_virtual_network" "hospital_vnet" {
+  name                = "VNet-Secure-IaC"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.hospital_rg.location
+  resource_group_name = azurerm_resource_group.hospital_rg.name
+}
+
+# 3. Subred para el Bastion
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.hospital_rg.name
+  virtual_network_name = azurerm_virtual_network.hospital_vnet.name
+  address_prefixes     = ["10.10.2.0/26"]
+}
+
+# 4. Subred para Máquinas Virtuales
+resource "azurerm_subnet" "workload_subnet" {
+  name                 = "Workload-Subnet"
+  resource_group_name  = azurerm_resource_group.hospital_rg.name
+  virtual_network_name = azurerm_virtual_network.hospital_vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
