14c8fcf61d773dfca1c8fb8df7d4482ead5f7d48
vickycastillo76-svg <vickycastillo76@icloud.com>
2026-05-04 17:33:35 +0200

    feat(terraform): implement bastion host and public ip for secure perimete

--- infrastructure-as-code-azure/main.tf
+++ infrastructure-as-code-azure/main.tf
@@ -28,3 +28,26 @@ resource "azurerm_subnet" "workload_subnet" {
   virtual_network_name = azurerm_virtual_network.hospital_vnet.name
   address_prefixes     = ["10.10.1.0/24"]
 }
+
+# 5. IP Pública para el Bastion (Requisito: SKU Standard)
+resource "azurerm_public_ip" "bastion_ip" {
+  name                = "IP-Bastion-IaC"
+  location            = azurerm_resource_group.hospital_rg.location
+  resource_group_name = azurerm_resource_group.hospital_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# 6. El Bastion Host (El túnel de seguridad)
+resource "azurerm_bastion_host" "hospital_bastion" {
+  name                = "Security-Bastion-IaC"
+  location            = azurerm_resource_group.hospital_rg.location
+  resource_group_name = azurerm_resource_group.hospital_rg.name
+
+  ip_configuration {
+    name      = "configuration"
+    subnet_id = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_ip.id
+  }
+}
+
